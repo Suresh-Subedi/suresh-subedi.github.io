ed35669a172a57d7fe96ec2697ff1abf2ae21cdd
Suresh-Subedi <7327694+Suresh-Subedi@users.noreply.github.com>
2023-04-13 16:07:05 +0200

    game wip

--- game/README.md
+++ game/README.md
@@ -1,2 +1,3 @@
 https://bevouliin.com/
+https://www.youtube.com/watch?v=c-1dBd1_G8A&ab_channel=Frankslaboratory
 https://www.youtube.com/watch?v=ug-gdfGb7I8&ab_channel=Frankslaboratory
--- game/main.js
+++ game/main.js
@@ -1,30 +1,53 @@
 import  { Player } from './player.js';
 import { InputHandler } from './input.js';
 import { Background } from './background.js';
+import { FlyingEnemy } from './enemies.js';
 
 window.addEventListener('load', function() {
     const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
-    canvas.width = 500;
+    canvas.width = 1980;
     canvas.height = 500;
 
     class Game {
         constructor(width, height) {
             this.width = width;
             this.height = height;
-            this.groundMargin = 50;
-            this.speed = 3;
+            this.groundMargin = 80;
+            this.speed = 0;
             this.background = new Background(this);
             this.player = new Player(this);
             this.input = new InputHandler();
+            this.enemies = [];
+            this.enemyTimer = 0;
+            this.enemyInterval = 1000;
         }
         update(deltaTime) {
             this.background.update();
             this.player.update(this.input.keys, deltaTime);
+            // handleEnemies
+            if(this.enemyTimer > this.enemyInterval) {
+                this.addEnemy();
+                this.enemyTimer = 0;
+            } else {
+                this.enemyTimer += deltaTime;
+            }
+            this.enemies.forEach(enemy => {
+                if(enemy.markedForDeletion) {
+                    enemies.splice(this.enemies.indexOf(enemy), 1);
+                }
+                enemy.update(deltaTime);
+            })
         }
         draw(context) {
             this.background.draw(context);
             this.player.draw(context);
+            this.enemies.forEach(enemy => {
+                enemy.draw(context);
+            })
+        }
+        addEnemy() {
+            this.enemies.push(new FlyingEnemy(this))
         }
     }
 
@@ -32,6 +55,12 @@ window.addEventListener('load', function() {
     let lastTime = 0;
 
     function animate(timeStamp) {
-
+        const deltaTime = timeStamp - lastTime;
+        lastTime = timeStamp;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        game.update(deltaTime);
+        game.draw(ctx);
+        requestAnimationFrame(animate);
     }
+    animate(0);
 });
